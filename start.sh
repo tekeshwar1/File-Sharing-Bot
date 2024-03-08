48a213d457b47dbff276eccfb777c0ffbaeea13d
tekeshwar1 <144202800+tekeshwar1@users.noreply.github.com>
2024-03-08 21:08:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/tekeshwar1/File-Sharing-Bot.git
+git clone https://github.com/tekeshwar1/File-Sharing-Bot.git /MissPerfectURLBOT
 cd /MissPerfectURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
